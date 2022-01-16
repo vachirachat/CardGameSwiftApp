a6953351d6d83f851b67bafcd140f8977018c0d3
Vachirachat.S <vachirachat@robowealth.co.th>
2022-01-16 14:12:33 +0700

    Initial Commit

---
+++ Shared/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  Shared
+//
+//  Created by Vachirachat Sawaddiwat Na Ayuttaya on 16/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
